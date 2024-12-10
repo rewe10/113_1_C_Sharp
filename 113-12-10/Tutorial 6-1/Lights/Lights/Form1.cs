b2b87917484c33d6bb315afd41729a0c27b80f27
rewe10 <a112340039@mail.shu.edu.tw>
2024-12-10 17:59:29 +0800

    113-12-10

---
+++ 113-12-10/Tutorial 6-1/Lights/Lights/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lights
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void turnLightON()
+        {
+            lightOnPictureBox.Visible = true;
+            lightOffPictureBox.Visible = false;
+            lightStateLabel.Text = "ON";
+        }
+        private void turnLightOFF()
+        {
+            lightOnPictureBox.Visible = false;
+            lightOffPictureBox.Visible = true;
+            lightStateLabel.Text = "OFF";
+        }
+        private void switchButton_Click(object sender, EventArgs e)
+        {
+            if (lightOffPictureBox.Visible == true)
+            {
+                turnLightOFF();
+            }
+            else
+            {
+                turnLightON();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
